95adc609a57fd695279ec4db7d178a95111dd7ad
herrmanno <O.herrmann92@gmail.com>
2015-10-13 10:27:44 +0200

    bug fix: onChange

--- src/onchange.ts
+++ src/onchange.ts
@@ -4,6 +4,7 @@ import { get } from 'horcrux-di';
 function onChange(clazz:typeof Store): MethodDecorator {
 	return (target: any, propertyKey: string | symbol, descriptor: any) => {
 		let store = get(clazz);
+		target.onCreated = target.onCreated || [];
 		target.onCreated.push(component => {
 			store.register(component[propertyKey], component);
 		});
